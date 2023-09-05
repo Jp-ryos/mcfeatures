34da0b02b797d89c6c67e17c4914a3ae6a1b5cd3
Jp-ryos <ubeat1001@gmail.com>
2023-09-05 21:15:57 +0900

    feat: add food [Kohlrabi]

--- src/main/java/jp/soyr/items/JpryosItems.java
+++ src/main/java/jp/soyr/items/JpryosItems.java
@@ -20,6 +20,9 @@ public class JpryosItems {
     public static final RegistryObject<Item> METAL_DETECTOR = ITEMS.register("metal_detector",
             () -> new MetalDetectorItem(new Item.Properties().durability(512)));
 
+    public static final RegistryObject<Item> KOHLRABI = ITEMS.register("kohlrabi",
+            () -> new Item(new Item.Properties().food(JpryosModFoodProperties.KOHLRABI)));
+
     public static void register(IEventBus iEventBus) {
         ITEMS.register(iEventBus);
     }
