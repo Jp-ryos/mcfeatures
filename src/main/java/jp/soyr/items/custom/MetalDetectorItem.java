6bc4f3532308985756af088ad672d430fb35f7d9
Jp-ryos <ubeat1001@gmail.com>
2023-09-05 21:52:26 +0900

    add: add custom Tooltips

--- src/main/java/jp/soyr/items/custom/MetalDetectorItem.java
+++ src/main/java/jp/soyr/items/custom/MetalDetectorItem.java
@@ -1,17 +1,23 @@
 package jp.soyr.items.custom;
 
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.client.resources.language.I18n;
 import net.minecraft.core.BlockPos;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
+import java.util.List;
 import java.util.Objects;
 
 public class MetalDetectorItem extends Item {
@@ -62,5 +68,16 @@ public class MetalDetectorItem extends Item {
 
     private boolean isValuableBlock(BlockState blockState) {
         return blockState.is(Blocks.IRON_ORE) || blockState.is(Blocks.DEEPSLATE_IRON_ORE) || blockState.is(Blocks.DIAMOND_BLOCK) || blockState.is(Blocks.DEEPSLATE_DIAMOND_ORE);
+    }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltipComponents, TooltipFlag pIsAdvanced) {
+        if (Screen.hasShiftDown()) {
+            pTooltipComponents.add(Component.translatable("tooltip.jpryosmod.metal_detector.tooltip.shift"));
+        } else {
+            pTooltipComponents.add(Component.translatable("tooltip.jpryosmod.metal_detector.tooltip"));
+        }
+
+        super.appendHoverText(pStack, pLevel, pTooltipComponents, pIsAdvanced);
     }
 }
